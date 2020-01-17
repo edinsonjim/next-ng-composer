3fe520e76c854c4faaffd4a2ffe685f6abfc529e
Edinson Jim Sanchez <edinsonjim@gmail.com>
2020-01-17 16:36:17 +0100

    fix: composer

--- composer.sh
+++ composer.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 echo "Next NG Composer"
 
+WORKDIR=$PWD
+
 echo "[INFO] Cloning next_ng_presentation" 
 git clone http://bitbucket.intranet.ifis.local/scm/fe/next_ng_presentation.git
 
 echo "[INFO] Cloning next_ng_frontend"
 git clone http://bitbucket.intranet.ifis.local/scm/fe/next_ng_frontend.git
 
+echo "[INFO] Pulling next_ng_presentation"
+cd "$WORKDIR/next_ng_presentation/" && git fetch && git checkout -b feature/mocks remotes/origin/feature/mocks
+cd "$WORKDIR/next_ng_presentation/it.ifis.nextng" && mvn clean package spring-boot:repackage -U
 
-cd next_ng_presentation && git fetch && git checkout -b feature/mocks remotes/origin/feature/mocks && cd it.ifis.nextng && mvn clean package spring-boot:repackage -U
-
-cd next_ng_frontend && git fetch && git checkout -b features/next-ng-fe remotes/origin/features/next-ng-fe && cd nextng-fe && npm i
+echo "[INFO] Pulling next_ng_presentation"
+cd "$WORKDIR/next_ng_frontend/" && git fetch && git checkout -b features/next-ng-fe remotes/origin/features/next-ng-fe 
+cd "$WORKDIR/next_ng_frontend/nextng-fe" && npm i
 
 echo "[INFO] Good Job!"

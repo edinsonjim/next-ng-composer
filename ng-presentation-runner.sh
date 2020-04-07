55bf90a61cd49c5810b088c0bd619fd05acde375
Edinson Jim Sanchez <edinsonjim@gmail.com>
2020-04-07 18:11:00 +0200

    chore: runner presentation

--- ng-presentation-runner.sh
+++ ng-presentation-runner.sh
@@ -1,13 +1,58 @@
 #!/bin/bash
+
 echo "Next NG Presentation Runner"
 
-cd next_ng_presentation/it.ifis.nextng/
+cd ./it.ifis.nextng
 
-if [[ "$1" == --build ]]
-then
+user="agollini"
+principalcreds="YWdvbGxpbmk6TWFyem8yMDIwOk9QRVJBVE9S"
+
+usage() {
+  echo "usage: bash runner.sh [-h|--help] [-u|--user <user>] [--build] "
+} 
+
+run_jar() {
+  echo "[RUNNER] [INFO] Running .jar with credentials $user:$principalcreds"
+  cd target && java -jar nextng-3.52.0-SNAPSHOT.jar --arch.presentation.auth.principalcreds=$principalcreds
+}
+
+define_credentials() {
+  case $user in
+    agollini )        principalcreds="YWdvbGxpbmk6TWFyem8yMDIwOk9QRVJBVE9S" ;;
+    pspadoni )        principalcreds="cHNwYWRvbmk6Y2xhcmFiZWxsYTE5NzcuOkNMSUVOVA==" ;;
+    ext.fmanici )     principalcreds="ZXh0LmZtYW5pY2k6R2VubmFpbzIwMjA6T1BFUkFUT1I=" ;;
+    ext.glagamba ) principalcreds="ZXh0LmdsYWdhbWJhOkZlYmJyYWlvMjAyMDpPUEVSQVRPUg==" ;;
+  esac
+} 
+
+build() {
+  echo "[RUNNER] [INFO] mvn clean install"
   mvn clean install spring-boot:repackage -U
-fi
+} 
 
-cd target && java -jar nextng-0.0.1-SNAPSHOT.jar --arch.presentation.auth.principalcreds=cHNwYWRvbmk6Y2xhcmFiZWxsYTE5Nzcu
+while [ "$1" != "" ]; do
+    case $1 in
+        -u | --user )           shift
+                                user=$1
+                                define_credentials
+                                ;;
+        -b | --build )          shift
+                                build
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+    esac
+    shift
+done
 
-echo "[INFO] Good Job!"
+run_jar
+
+# ext.fmanici:OPERATOR
+# ZXh0LmZtYW5pY2k6R2VubmFpbzIwMjA6T1BFUkFUT1I=
+
+# pspadoni:CLIENT
+# cHNwYWRvbmk6Y2xhcmFiZWxsYTE5NzcuOkNMSUVOVA==
+
+# agollini:Marzo2020:OPERATOR
+# YWdvbGxpbmk6TWFyem8yMDIwOk9QRVJBVE9S

13cf8ea509c0192fc8a0819be1c811cdee092eb3
Edinson Jim Sanchez <edinsonjim@gmail.com>
2020-06-04 20:02:52 +0200

    refactor: branch base

--- composer.sh
+++ composer.sh
@@ -14,11 +14,10 @@ echo "[INFO] Cloning next_ng_frontend"
 git clone http://bitbucket.intranet.ifis.local/scm/fe/next_ng_frontend.git
 
 echo "[INFO] Pulling next_ng_presentation"
-cd "$WORKDIR/next_ng_presentation/" && git fetch && git checkout -b feature/mocks remotes/origin/feature/mocks
-cd "$WORKDIR/next_ng_presentation/it.ifis.nextng" && mvn clean package spring-boot:repackage -U
+cd "$WORKDIR/next_ng_frontend/" && git fetch && git checkout -b features/next-ng-fe origin/features/next-ng-fe
+cd "$WORKDIR/next_ng_frontend/nextng-fe" && npm install
 
 echo "[INFO] Pulling next_ng_presentation"
-cd "$WORKDIR/next_ng_frontend/" && git fetch && git checkout -b features/next-ng-fe remotes/origin/features/next-ng-fe 
-cd "$WORKDIR/next_ng_frontend/nextng-fe" && npm i
+cd "$WORKDIR/next_ng_presentation/" && git fetch && git checkout -b features/next-ng-fe origin/features/next-ng-fe
 
 echo "[INFO] Good Job!"
